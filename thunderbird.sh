dd7d5e833b3c8bfce721246040861de7b4241340
Stephen Meech <stephen@smeech.plus.com>
2021-06-25 22:16:04 +0100

    wmctrl -l -x to check class for "Mail.Thunderbird" which is less likely to pick up non-Thunderbird windows.

--- thunderbird.sh
+++ thunderbird.sh
@@ -2,9 +2,9 @@
 
 thunderbird "$@" &
 
-until wmctrl -l | grep -q 'Mozilla Thunderbird'; do sleep 1; done
+until wmctrl -l -x | grep -q 'Mail.Thunderbird'; do sleep 1; done
 
-wid=$(wmctrl -l | grep Thunderbird | awk '{print $1;}')
+wid=$(wmctrl -l -x | grep 'Mail.Thunderbird' | awk '{print $1;}')
 
 wmctrl -ir "$wid" -b remove,maximized_vert,maximized_horz
 wmctrl -ir "$wid" -e '0,1280,0,-1,-1'
