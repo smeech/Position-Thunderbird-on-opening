0fbcb9b7ad833bef0065cf4709f85127ba5cbeeb
Stephen Meech <stephen@smeech.plus.com>
2021-06-26 09:46:36 +0100

    Replaced awk '{print $1;}' with the lighter cut -f1 -d' ' as suggested by dave0808

--- thunderbird.sh
+++ thunderbird.sh
@@ -4,7 +4,7 @@ thunderbird "$@" &
 
 until wmctrl -l -x | grep -q 'Mail.Thunderbird'; do sleep 1; done
 
-wid=$(wmctrl -l -x | grep 'Mail.Thunderbird' | awk '{print $1;}')
+wid=$(wmctrl -l -x | grep 'Mail.Thunderbird' | cut -f1 -d' ')
 
 wmctrl -ir "$wid" -b remove,maximized_vert,maximized_horz
 wmctrl -ir "$wid" -e '0,1280,0,-1,-1'
